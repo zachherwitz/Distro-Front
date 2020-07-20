711059fe8df5d597c70bdec4ccf25e33e97dc386
Zach Herwitz <zach.herwitz@gmail.com>
2020-07-20 00:38:16 -0400

    more styling

--- src/components/users/SingleUserDisplay.js
+++ src/components/users/SingleUserDisplay.js
@@ -1,19 +1,25 @@
 import React from 'react'
+import Weather from './Weather'
 
 class SingleUserDisplay extends React.Component {
   render = () => {
     // console.log(JSON.stringify(this.props.callsheet));
-    return <div>
-      <h2>{this.props.user.name}</h2>
-      <div>
-        {this.props.callsheet ?
-          <div>
-            <h3>DATE: {this.props.callsheet.date}</h3>
-            <h3>PROJECT: {this.props.callsheet.projectTitle}</h3>
-          </div> : null }
-        <h4>CALL: {this.props.user.callsheet.callTime}</h4>
-        <h4>REPORT TO {this.props.user.callsheet.location}</h4>
+    return <div className="user-role-view">
+      <div className="user-left"></div>
+      <div className="user-information">
+        <h2>{this.props.user.name}</h2>
+        <div>
+          {this.props.callsheet ?
+            <div>
+              <h3>DATE: {this.props.callsheet.date}</h3>
+              <h3>PROJECT: {this.props.callsheet.projectTitle}</h3>
+            </div> : null }
+          <h4>CALL: {this.props.user.callsheet.callTime}</h4>
+          <h4>REPORT TO {this.props.user.callsheet.location}</h4>
+          {this.props.callsheet ? <Weather callsheet={this.props.callsheet}/> : null}
+        </div>
       </div>
+      <div className="user-right"></div>
     </div>
   }
 }
